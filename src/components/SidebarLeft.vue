60942e8a8c2d318589517e651d64849529bee99d
WhiteLeaf03 <2876202234@qq.com>
2022-09-05 15:21:57 +0800

    新增左侧边栏
    新增用户卡片

--- src/components/SidebarLeft.vue
+++ src/components/SidebarLeft.vue
@@ -1,13 +1,23 @@
 <template>
-
+  <div id="SidebarLeft">
+    <UserCard></UserCard>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "SidebarLeft"
-}
+  import UserCard from "./SidebarLeft/UserCard";
+  export default {
+    name: "SidebarLeft",
+    components: {UserCard},
+    mounted() {
+      document.getElementById("SidebarLeft").style.marginLeft = `${innerWidth * 0.2}px`
+      document.getElementById("SidebarLeft").style.marginRight = `0px`
+    }
+  }
 </script>
 
 <style scoped>
-
+  #SidebarLeft {
+    width: 260px;
+  }
 </style>
---
+++ src/components/SidebarLeft/UserCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="UserCard">
+    <img src="images/avatar.jpg" id="avatar" alt="ERROR">
+    <p id="username">WhiteLeaf03</p>
+    <p id="introduce">一枚渣渣程序员</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UserCard"
+  }
+</script>
+
+<style scoped>
+  #UserCard {
+    position: relative;
+    background-color: rgba(255, 255, 255, 0.5);
+    padding: 16px 32px;
+    border-radius: 10px;
+  }
+
+  #avatar {
+    display: inline-block;
+    border-radius: 1000px;
+    width: 54px;
+    height: 54px;
+    vertical-align: middle;
+    margin-right: 16px;
+  }
+
+  #username {
+    display: inline-block;
+    font-family: "Copperplate Gothic Light", serif;
+    height: 54px;
+    line-height: 54px;
+    font-size: medium;
+    font-weight: bolder;
+  }
+
+  #introduce {
+    margin-top: 16px;
+    text-align: right;
+  }
+</style>
