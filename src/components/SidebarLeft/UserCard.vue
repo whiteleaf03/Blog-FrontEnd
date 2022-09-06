07a5269255802911293c035d65667db51b428eac
WhiteLeaf03 <2876202234@qq.com>
2022-09-06 11:16:08 +0800

    部分图片由本地链接转为图床链接

--- src/components/SidebarLeft/UserCard.vue
+++ src/components/SidebarLeft/UserCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="UserCard">
-    <img src="images/avatar.jpg" id="avatar" alt="ERROR">
+    <img src="https://s1.ax1x.com/2022/09/06/v7yQcd.jpg" id="avatar" alt="ERROR">
     <p id="username">WhiteLeaf03</p>
     <p id="introduce">一枚渣渣程序员</p>
   </div>
