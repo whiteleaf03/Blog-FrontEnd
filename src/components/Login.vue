a178e50afaf0959b667fcf66cbc9d27b183840c9
WhiteLeaf03 <2876202234@qq.com>
2022-10-10 13:35:11 +0800

    新增登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="Login">
+    <div id="title">
+      登 录
+    </div>
+    <div class="line">
+      <div class="tips">用户名</div>
+      <div class="input"><input v-model="username" type="text"></div>
+    </div>
+    <div class="line">
+      <div class="tips">密码</div>
+      <div class="input"><input v-model="password" type="password"></div>
+    </div>
+    <div class="line">
+      <div style="width: 360px; text-align: center">
+        <input class="button" type="button" value="can can need">
+      </div>
+    </div>
+    <div class="line">
+      <div style="width: 360px; text-align: center">
+        <router-link to="/home"><input class="button" type="button" value="愿风神忽悠你"></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        username: '',
+        password: '',
+        valid: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #Login {
+    backdrop-filter: blur(25px);
+    width: 640px;
+    height: auto;
+    margin: 120px auto 0;
+    border: 3px black solid;
+    border-radius: 15px;
+  }
+
+  #title {
+    text-align: center;
+    font-size: 56px;
+    margin-top: 16px;
+  }
+
+  .line {
+    margin: 32px auto;
+    width: 360px;
+  }
+
+  input {
+    width: 360px;
+    border-radius: 5px;
+    border: 1px solid black;
+    padding: 6px;
+    font-family: 阿里巴巴普惠体-regular;
+  }
+
+  input:focus {
+    outline: 1px solid black;
+  }
+
+  .button {
+    width: 180px;
+    margin: 0px auto 0;
+    font-family: 包圆小白体, fangsong;
+  }
+
+  #back {
+    width: 64px;
+    height: 64px;
+    position: absolute;
+    right: 16px;
+    top: 16px;
+    border: 3px solid black;
+    border-radius: 5px;
+  }
+</style>
