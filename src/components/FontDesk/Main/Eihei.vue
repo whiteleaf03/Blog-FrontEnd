3734798c284eacce8f5bd401dd87467c85194f05
WhiteLeaf03 <2876202234@qq.com>
2022-10-14 09:55:30 +0800

    使用ElementPlus

--- src/components/FontDesk/Main/Eihei.vue
+++ src/components/FontDesk/Main/Eihei.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="Eihei">
     <div id="Recommend">
-      <div class="title">站☆长☆推☆荐</div>
-      <div id="banner">
-        <ul id="img-list">
-          <li><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></li>
-          <li><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></li>
-          <li><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></li>
-          <li><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></li>
-        </ul>
+      <div id="Recommend-title">站☆长☆推☆荐</div>
+      <div id="Recommend-banner">
+        <el-carousel height="400px" indicator-position="outside">
+          <el-carousel-item><a :href="this.url"><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+          <el-carousel-item><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+          <el-carousel-item><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+          <el-carousel-item><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+          <el-carousel-item><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+          <el-carousel-item><a href=""><div class="banner-image"><div class="banner-info">[歌曲MV]如果突然想起我</div></div></a></el-carousel-item>
+        </el-carousel>
       </div>
     </div>
   </div>
@@ -19,64 +21,32 @@
     name: 'Eihei',
     data() {
       return {
-        left: 12,
-        timer: 0,
-        bannerWidth: 0,
-        step: 150
+        url: "www.baidu.com"
       }
     },
     mounted() {
-      this.runBanner()
+      this.initBanner()
     },
     methods: {
-      runBanner() {
-        if (this.left <= -1800) {
-          this.left = 0;
+      async initBanner() {
+        let bannerWidth = document.getElementById('Recommend-title').clientWidth
+        for (let index in document.getElementsByClassName('banner-image').length) {
+          document.getElementsByClassName('banner-image')[index].style.width = `${bannerWidth}px`
         }
-        document.getElementById('img-list').style.marginLeft = `${this.left}px`
-        let n
-        n = (this.left % 600 === 0) ? n = 2500 : n = 10
-        this.timer = setTimeout(this.runBanner, n)
-        this.left -= 6
       }
     }
   }
 </script>
 
 <style scoped>
-  #Eihei {
-
-  }
-
-  #Recommend {
-    padding: 0 16px;
-  }
-
-  .title {
+  #Recommend-title {
     font-family: 包圆小白体, fangsong;
     font-size: 42px;
     text-align: center;
   }
 
-  #banner {
-    width: 600px;
-    height: 338px;
-    overflow: hidden;
-    margin: 32px auto 0;
-    position: relative;
-  }
-
-  #img-list {
-    width: 2400px;
-    height: 338px;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-  }
-
-  #img-list li {
-    float: left;
+  #Recommend-banner {
+    margin-top: 16px;
   }
 
   a, a:visited {
@@ -84,8 +54,7 @@
   }
 
   .banner-image {
-    width: 600px;
-    height: 338px;
+    height: 400px;
     position: relative;
     background-image: url("images/test/Recommend-001.png");
     background-size: cover;
@@ -103,34 +72,5 @@
     font-size: 24px;
     background-color: whitesmoke;
     padding: 8px 16px;
-  }
-
-  .Recommend-Item {
-    display: flex;
-    background-color: whitesmoke;
-    margin: 32px 0;
-  }
-
-  .Recommend-Item-cover {
-    width: 320px;
-    height: 180px;
-    display: inline-block;
-    margin-right: 32px;
-    flex: 1;
-  }
-
-  .Recommend-Item-title {
-    flex: 2;
-    display: flex;
-    text-align: center;
-  }
-
-  .Recommend-Item-class {
-    flex: 3;
-    text-align: center;
-  }
-
-  .Recommend-Item-link {
-    flex: 3;
   }
 </style>
