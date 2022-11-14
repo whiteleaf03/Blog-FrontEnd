f1765334d886e0458cc85c61ed3cf8652c4b999f
WhiteLeaf03 <2876202234@qq.com>
2022-11-14 13:56:25 +0800

    fix

--- src/components/BackStage/BSEihei.vue
+++ src/components/BackStage/BSEihei.vue
@@ -1,13 +1,45 @@
 <template>
-
+  <div id="BSEihei">
+    <div id="Recommend">
+      <div class="Recommend-input">
+        <div class="title">推荐标题</div>
+        <input v-model="title">
+      </div>
+      <div class="Recommend-input">
+        <div class="title">封面路径</div>
+        <input v-model="cover">
+      </div>
+      <div class="Recommend-input">
+        <div class="title">资源类型</div>
+        <input v-model="type">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "BSEihei"
+    name: "BSEihei",
+    data() {
+      return {
+        title: "",
+        cover: "",
+        type: ""
+      }
+    }
   }
 </script>
 
 <style scoped>
+  #Recommend {
+    display: flex;
+    height: 64px;
+  }
 
+  .Recommend-input {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    margin: 32px;
+  }
 </style>
