2cb7ecf5a9336f1831fb7b7c5b2f44263cc6cad5
WhiteLeaf03 <2876202234@qq.com>
2022-11-15 19:53:48 +0800

    fix

--- src/request/BackDeskRequest.js
+++ src/request/BackDeskRequest.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {getCookie} from "../utils.js";
 
 //登录请求
 export async function login(username, password) {
@@ -12,6 +13,92 @@ export async function login(username, password) {
         data: JSON.stringify({
             username: username,
             password: password
+        })
+    }).then((response) => {
+        result = response.data;
+    })
+    return result;
+}
+
+export async function getAnalysis() {
+    let result;
+    await axios({
+        url: '/bs/api/analysis',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': getCookie('token')
+        }
+    }).then((response) => {
+        result = response.data;
+    })
+    return result;
+}
+
+export async function getSaysay() {
+    let result;
+    await axios({
+        url: '/bs/api/saysay',
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json',
+            token: getCookie('token')
+        }
+    }).then((response) => {
+        result = response.data;
+    })
+    return result;
+}
+
+export async function postSaysay(date, text) {
+    let result;
+    await axios({
+        url: '/bs/api/saysay',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            token: getCookie('token')
+        },
+        data: JSON.stringify({
+            date: date,
+            text: text
+        })
+    }).then((response) => {
+        result = response.data;
+    })
+    return result;
+}
+
+export async function putSaysay(id, text) {
+    let result;
+    await axios({
+        url: '/bs/api/saysay',
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json',
+            token: getCookie('token')
+        },
+        data: JSON.stringify({
+            id: id,
+            text: text
+        })
+    }).then((response) => {
+        result = response.data;
+    })
+    return result;
+}
+
+export async function deleteSaysay(id) {
+    let result;
+    await axios({
+        url: '/bs/api/saysay',
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json',
+            token: getCookie('token')
+        },
+        data: JSON.stringify({
+            id: id
         })
     }).then((response) => {
         result = response.data;
