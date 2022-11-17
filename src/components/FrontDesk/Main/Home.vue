9a90c7dd2aeba9fc034278da1d9b8568955c2032
WhiteLeaf03 <2876202234@qq.com>
2022-11-17 14:02:36 +0800

    完善页面

--- src/components/FrontDesk/Main/Home.vue
+++ src/components/FrontDesk/Main/Home.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div id="Home">
     <p>欢迎来到风之屿！</p>
-    <p>这是一个二刺猿码农小站</p>
-    <p>平时写点日记和说说</p>
-    <p>还有学习笔记</p>
+    <p>一个二刺猿码农的小破站</p>
+    <p>平时写点笔记和说说</p>
     <p>欢迎留言吖~</p>
+    <img src="/images/touch-fish.png">
+    <p>诶嘿~</p>
   </div>
 </template>
 
@@ -15,5 +16,26 @@
 </script>
 
 <style scoped>
+  #Home {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 
+  #Home p {
+    flex: 1;
+    text-align: center;
+    font-family: Tanugo糖果手写体, fangsong;
+    font-size: xx-large;
+    font-weight: bold;
+  }
+
+  #Home img {
+    height: 50%;
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
